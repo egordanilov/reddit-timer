b532dbc1e0b036810c3e55352f64d81cdfe6dc04
Egor Danilov <egordanilov970@gmail.com>
2022-09-11 00:04:36 -0500

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       App Placeholder
-    </div>
+    </>
   );
 }
 
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        color: red;
+    }
+`;
+
+export default GlobalStyles;
