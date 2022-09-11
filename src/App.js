428d0f49785ef297590831d109c8e1321f3efb4c
Egor Danilov <egordanilov970@gmail.com>
2022-09-11 00:10:46 -0500

    Reset default browser styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
+import CssReset from './styles/CssReset';
 
 function App() {
   return (
     <>
+      <CssReset />
       <GlobalStyles />
       App Placeholder
     </>
