e5e58524dcc10bef172c13e7157aa2ab3478f7df
Egor Danilov <egordanilov970@gmail.com>
2022-09-11 02:31:21 -0500

    Add router with routes / and /search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Search from './Search';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      App Placeholder
+      <header>Header</header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      <footer>Footer</footer>
     </>
   );
 }
