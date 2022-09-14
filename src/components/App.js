cd56890f93d728cd0a4919263795ed75148f0ffa
Egor Danilov <egordanilov970@gmail.com>
2022-09-13 20:15:33 -0500

    Review, fixed font capability, fonts using woff and woff2 now, added ThemeProvider wrapper with theme file to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/GlobalStyles';
+import theme from '../styles/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <header>Header</header>
       <Outlet />
       <footer>Footer</footer>
-    </>
+    </ThemeProvider>
   );
 }
 
