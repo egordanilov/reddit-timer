0761897820a908e4a5dfc67da0645cb227e26d31
Egor Danilov <egordanilov970@gmail.com>
2022-09-14 15:56:56 -0500

    Adjustments to Header component position, after tests failed for useRoutes

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../styles/GlobalStyles';
-import Header from '../UI/Header';
 import theme from '../styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header />
       <header>Header</header>
       <Outlet />
       <footer>Footer</footer>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Search from './pages/Search';
+import Header from './UI/Header';
 import Home from './pages/Home';
 import App from './components/App';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
   <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="search" element={<Search />} />
