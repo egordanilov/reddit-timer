fc2e82473eb60e53c55400795ab5737ef679e8f7
egordanilov <egordanilov970@gmail.com>
2022-09-19 05:38:31 -0600

    Logo imported, no styling, refactoring still needed

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FooterStyled from '../styles/FooterStyled.style';
+import { ReactComponent as IconMenu } from './footerlogo.svg';
 
 function Footer() {
   return (
     <FooterStyled>
-      Styled footer
+      <div>
+        <a href="https://ooloo.io/employers">Profy.dev</a>
+      </div>
+      <div><Link to="/"><IconMenu /></Link></div>
+      <div><Link to="/terms">Terms &amp; Privacy</Link></div>
     </FooterStyled>
   );
 }
--- src/styles/FooterStyled.style.js
+++ src/styles/FooterStyled.style.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
 const FooterStyled = styled.footer`
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    left: 0;
+    bottom: 0;
 `;
 
 export default FooterStyled;
