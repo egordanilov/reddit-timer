1078073be4438c59b6ec61e6b6f4c98628f3b3b7
unknown <egordanilov970@gmail.com>
2022-09-25 02:19:19 -0500

    Subreddit form component and styled comp initialized and imported into Search page

---
+++ src/components/SubredditForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SubredditFormWrapper from '../styles/SubredditFormWrapper.style';
+
+function SubredditForm() {
+  return (
+    <SubredditFormWrapper>
+      Subreddit form goes here
+    </SubredditFormWrapper>
+  );
+}
+
+export default SubredditForm;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import SubredditForm from '../components/SubredditForm';
 
 export default function Search() {
   const { subreddit = 'javascript' } = useParams();
   return (
     <section className="viewWrapper">
+      <SubredditForm />
       Search page
       {subreddit}
     </section>
