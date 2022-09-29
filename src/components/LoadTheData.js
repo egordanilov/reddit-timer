21e0f7bc360da9c327bc37bdf91fcf2a6be09678
egordanilov <egordanilov970@gmail.com>
2022-09-28 19:35:36 -0600

    Fetch has been implemented, raw data, very basic step

---
+++ src/components/LoadTheData.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import defaultSubreddit from '../sharedVariables';
+/* eslint-disable */
+
+function LoadTheData() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const { subreddit = defaultSubreddit} = useParams();
+
+
+  useEffect(() => {
+    console.log('useEffect has been running, subreddit is',subreddit);
+    setIsLoaded(false);
+    setPosts([]);
+    setError(null);
+    fetch(`https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100`)
+      .then((res) => res.json())
+      .then((data) => 
+        {
+        setIsLoaded(true);
+        setPosts(data.data.children);
+        console.log(data.data.children);
+        },
+        (error) => {
+            setIsLoaded(true);
+            setError(error);
+        }
+      );
+  }, [subreddit]);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (JSON.stringify(posts));
+  }
+}
+
+
+export default LoadTheData;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SubredditForm from '../components/SubredditForm';
+import LoadTheData from '../components/LoadTheData';
 
 export default function Search() {
   return (
     <section className="viewWrapper">
       <SubredditForm />
+      <LoadTheData />
     </section>
   );
 }
