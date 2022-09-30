31d64a14d068d2d130aa135692e7c94d658eef9b
Egor Danilov <egordanilov970@gmail.com>
2022-09-30 16:02:53 -0500

    HeatMap component implemented

---
+++ src/components/Heatmap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeatMapWrapper from '../styles/HeatmapWrapper.style';
+
+function HeatMap() {
+  return (
+    <HeatMapWrapper>
+      Heatmap goes here
+    </HeatMapWrapper>
+  );
+}
+
+export default HeatMap;
---
+++ src/styles/HeatMapWrapper.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const HeatMapWrapper = styled.section`
+`;
+
+export default HeatMapWrapper;
