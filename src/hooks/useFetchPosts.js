e1b30c13709d27248690cce08381caa622bd3627
Egor Danilov <egordanilov970@gmail.com>
2022-10-06 13:36:29 -0500

    useFetchPosts hook now returns structured post list

--- src/hooks/useFetchPosts.js
+++ src/hooks/useFetchPosts.js
@@ -1,4 +1,25 @@
 import { useEffect, useState } from 'react';
+/* eslint-disable */
+function sortPostList(unsortedList) {
+  const sortedList = unsortedList.map((post) => {
+    const postDate = new Date(post.data.created_utc * 1000);
+    const postDay = postDate.getDay();
+    const postHour = postDate.getHours(); 
+    const newPost = {
+      title: post.data.title,
+      created_utc: post.data.created_utc,
+      postDay,
+      postHour,
+      upvotes: post.data.ups,
+      author: post.data.author,
+      num_comments: post.data.num_comments,
+      url: post.data.url,
+      author_is_blocked: post.data.author_is_blocked,
+    }
+    return newPost;
+  });
+  return sortedList;
+}
 
 async function fetchPaginatedPosts(subreddit, previousPosts = [], after = null) {
   let url = `https://www.reddit.com/r/${subreddit}/top.json?t=year&limit=100`;
@@ -7,10 +28,11 @@ async function fetchPaginatedPosts(subreddit, previousPosts = [], after = null) 
   }
   const response = await fetch(url);
   const { data } = await response.json();
-  const allPosts = previousPosts.concat(data.children);
+  let allPosts = previousPosts.concat(data.children);
   const noMorePosts = data && data.dist < 100;
   const limitReached = allPosts.length >= 500;
   if (noMorePosts || limitReached) {
+    allPosts = sortPostList(allPosts);
     return allPosts;
   }
   return fetchPaginatedPosts(subreddit, allPosts, data.after);
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -11,7 +11,12 @@ export default function Search() {
   const fetchPosts = useFetchPosts(subreddit);
   let fetchOutput = <></>;
   if (fetchPosts.error) {
-    fetchOutput = <>Failed to fetch, check internet connection and subreddit name</>;
+    fetchOutput = (
+      <>
+        Failed to fetch, check internet connection and subreddit name
+        {fetchPosts.error}
+      </>
+    );
   }
   if (!fetchPosts.isLoaded) {
     fetchOutput = <LoadingSpinner />;
