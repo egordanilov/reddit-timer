345e91512c432e9b19b4cefe1e4c2ea1af9e868f
egordanilov <egordanilov970@gmail.com>
2022-10-13 10:09:43 -0500

    Separated HeatMap component, need to fix structure, tests failed for useFetchPosts

--- src/components/HoursByWeekDayHour.js
+++ src/components/HoursByWeekDayHour.js
@@ -4,12 +4,23 @@ import * as S from '../styles/HeatMapWrapper.style';
 import { getPostsByDayHour } from '../hooks/useFetchPosts';
 import { utcHours } from '../sharedVariables';
 
-function HoursByWeekDayHour({ weekDay, listOfPosts = [], clickHandler }) {
+function HoursByWeekDayHour(
+  {
+    weekDay, listOfPosts = [], clickHandler, activeHour,
+  },
+) {
   const parsedHours = utcHours.map((hourOfTheDay) => {
     const postsByDayHour = getPostsByDayHour(listOfPosts, weekDay, hourOfTheDay);
     const numberOfPosts = postsByDayHour.length;
     return (
-      <S.HeatMapRowNumberOfPosts key={`${weekDay} ${hourOfTheDay}`} onClick={() => { clickHandler(weekDay, hourOfTheDay); }} numberOfPosts={numberOfPosts} role="button" tabindex={-1}>
+      <S.HeatMapRowNumberOfPosts
+        key={`${weekDay} ${hourOfTheDay}`}
+        onClick={() => { clickHandler(weekDay, hourOfTheDay); }}
+        numberOfPosts={numberOfPosts}
+        role="button"
+        tabindex={-1}
+        selected={activeHour === hourOfTheDay}
+      >
         {numberOfPosts}
       </S.HeatMapRowNumberOfPosts>
     );
@@ -27,6 +38,8 @@ HoursByWeekDayHour.propTypes = {
   /* eslint-disable-next-line */
   listOfPosts: array.isRequired,
   clickHandler: func.isRequired,
+  /* eslint-disable-next-line */
+  activeHour: string.isRequired,
 };
 
 export default HoursByWeekDayHour;
--- src/components/WeekdayRows.js
+++ src/components/WeekdayRows.js
@@ -1,14 +1,20 @@
+/* eslint-disable */
 import React from 'react';
 import { array, func } from 'prop-types';
 import * as S from '../styles/HeatMapWrapper.style';
 import { weekdays } from '../sharedVariables';
 import HoursByWeekDayHour from './HoursByWeekDayHour';
 
-function WeekdayRows({ listOfPosts, clickHandler }) {
+function WeekdayRows({ listOfPosts, clickHandler, activeCell }) {
   const parsedWeekdays = weekdays.map((weekday) => (
     <S.HeatMapRow key={weekday}>
       <S.HeatMapRowWeekday>{weekday}</S.HeatMapRowWeekday>
-      <HoursByWeekDayHour listOfPosts={listOfPosts} weekDay={weekday} clickHandler={clickHandler} />
+      <HoursByWeekDayHour
+        listOfPosts={listOfPosts}
+        weekDay={weekday}
+        clickHandler={clickHandler}
+        activeHour={activeCell.day === weekday ? activeCell.hour : null}
+      />
     </S.HeatMapRow>
   ));
   return (
