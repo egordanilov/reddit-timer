81daead40877ea9f55816113b90ed0de4e3dc5ce
Egor Danilov <egordanilov970@gmail.com>
2022-10-22 00:58:08 -0500

    Fixed reddit user link from reddit.com/user/{user} to reddit.com/u/{user}

--- src/components/PostAuthor.js
+++ src/components/PostAuthor.js
@@ -3,7 +3,7 @@
 import React from 'react';
 
 function PostAuthor({ post }) {
-    return <a href={`https://reddit.com/user/${post.author}`}>{post.author}</a>;
+    return <a href={`https://reddit.com/u/${post.author}`}>{post.author}</a>;
 }
 
 export default PostAuthor;
