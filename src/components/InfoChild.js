5690906d909c70d9d446ff758f05b5bb7aa86b14
egordanilov <egordanilov970@gmail.com>
2022-10-24 00:39:22 -0500

    InfoSection component naming made more clear

--- src/components/InfoChild.js
+++ src/components/InfoChild.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { string, node } from 'prop-types';
 import * as S from '../styles/InfoSectionWrapper.style';
 
-function Info({ id, headline, children }) {
+function InfoChild({ id, headline, children }) {
   return (
     <S.InfoStyled id={id}>
       <S.InfoSectionHeader>{ headline }</S.InfoSectionHeader>
@@ -11,10 +11,10 @@ function Info({ id, headline, children }) {
   );
 }
 
-Info.propTypes = {
+InfoChild.propTypes = {
   id: string.isRequired,
   headline: string.isRequired,
   children: node.isRequired,
 };
 
-export default Info;
+export default InfoChild;
